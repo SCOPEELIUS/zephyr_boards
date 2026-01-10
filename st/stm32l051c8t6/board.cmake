38f8f0cd3c3a76d358cdb191da95268ca859485e
SCOPEELIUS <scopeelius@gmail.com>
2026-01-10 18:18:57 +0300

    STM32L051x8

--- st/stm32l051c8t6/board.cmake
+++ st/stm32l051c8t6/board.cmake
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # keep first
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=sw")
 board_runner_args(jlink "--device=STM32L051C8T6" "--speed=4000")
+board_runner_args(openocd "--cmd-pre-init" "source [find interface/stlink.cfg]" "--cmd-pre-init" "source [find target/stm32l0.cfg]" "--cmd-pre-init" "reset_config none separate")
+board_runner_args(openocd "--gdb-init" "set pagination off")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
